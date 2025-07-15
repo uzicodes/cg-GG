dbfaca4feec0e7ea152429ecbea99612f9e4f5d7
Utsho Heaven Chowdhury <utshozi11@gmail.com>
2025-07-16 00:33:04 +0600

    name modified to cg-GG

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:cg_gg/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const CGPACalculatorApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
